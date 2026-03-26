e1679bb1bb9ad02a063b92f52ffa7b5c7b1c2f6f
sam floyd <cf209380@umconnect.umt.edu>
2026-03-26 15:16:49 -0600

    Hw 9

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -1,69 +1,88 @@
-let idleAnimation;
-let walkAnimation;
-let dogImage;
-let idlePaths = [];
-let walkPaths = [];
-let myAnimation;
+//sprite objects
+let dog;
+let dogAni;
+let bones;
+let logs;
+let bees;
 
+//image objects
+let boneImg;
+let logImg;
+let beeImg;
+
+//score 
+let score;
+
+
+/////////////////////////////////////////////////////////////
 function preload() {
-    
-    for(var i = 0; i < 10; i++)    {
-        idlePaths[i] = 'assets/dogidle/' +  'frame' + i + '.png';
-       
-    }
-    for(var j = 0; j < 8; j++)    {
-        walkPaths[j] = 'assets/dogwalk/' +  'frame' + i + '.png';
-       
-    }
-
-      
-}
-
-function setup() {
   
-    new Canvas(1000, 400);
-    
-    //compact way to add an image
-  	dogImage = new Sprite();
-	dogImage.image = 'assets/dog.png';
-    dogImage.pos.x = 250;
-    dogImage.pos.y = 200;
-    //dogImage.scale = 0.05;
-    dogImage.width = 100;
-    dogImage.height = 100;
-    dogImage.collider = 'rectangle';
-    dogImage.debug = true;
-
-
-    //myAnimation = new Sprite();
-	//myAnimation.scale = 0.5;
-    
- 
-    idleAnimation = loadAni('assets/dogidle/frame0.png', 7);
-
-   // idleAnimation = loadAni(idlePaths[0], idlePaths[1], idlePaths[2], idlePaths[3], 
-   // idlePaths[4], idlePaths[5],idlePaths[6], idlePaths[7], idlePaths[8],
-   // idlePaths[9], idlePaths[10], idlePaths[11], idlePaths[12], idlePaths[13], idlePaths[14]);
- 
-    //idleAnimation = loadAni('idle', idlePaths);
-    //walkAnimation = loadAni('walk', walkPaths);
-    
-    //myAnimation.addAni('idle', idlePaths);
-    //myAnimation.addAni('walk', walkPaths);
-    //myAnimation.frameDelay = 20;
-//myAnimation.frameDelay = 1;
-    
-    myAnimation = new animationImage(100,300, 150, 150);
-    myAnimation.loadAnimation('idle', idlePaths);
-    myAnimation.loadAnimation('walk', walkPaths);
-    myAnimation.debug = true;
+  dogAni = loadAnimation(imageSequence("assets/dogrun/frame", 8));
+  
+  boneImg = loadImage("assets/bone.png");
+  
+  logImg = loadImage("assets/log.png");
+  
+  beeImg = loadImage("assets/bee.png");
+  
 }
-// display all the frames using the draw function as a loop
-function update() 
-{
 
-    clear();
+////////////////////////////////////////////////////////////
+function setup() {
 
+  createCanvas(1000, 400);
+  
+  //sprite
+  dog = new Sprite();
+  dog.addAni(dogAni);
+  dog.scale = 0.25;
+  dog.w = 35;
+  dog.h = 75;
+  dog.rotationLock = true;
+  dog.debug = true;
+  dog.health = 5;
+  
+  //collectables
+  bones = new Group();
+  bones.image = boneImg;
+  bones.scale = 0.05;
+  bones.x = () => random(width);
+  bones.y = () => random(height);
+  bones.diameter = boneImg.width * 0.8;
+  bones.amount = 10;
+  
+  dog.collides(bones, boneHit);
+  
+  //obstacles
+  logs = new Group();
+  logs.image = logImg;
+  logs.scale = 0.01;
+  logs.x = () => random(width);
+  logs.y = () => random(height);
+  logs.diameter = logImg.width * 0.8;
+  logs.amount = 5;
+  
+  dog.collides(logs, logHit);
+  
+  //enemies
+  bees = new Group();
+  bees.image = beeImg;
+  bees.scale = 0.05;
+  bees.x = () => random(width);
+  bees.y = () => random(height);
+  bees.diameter = beeImg.width * 0.8;
+  bees.amount = 5;
+  
+  dog.collides(bees, beeHit);
+  
+  //score
+  score = 0;
+  
+}
+
+////////////////////////////////////////////////////////////
+function draw() {
+  
   //background
   background(163,212,255);
   
@@ -76,78 +95,62 @@ function update()
   noStroke();
   fill(92,184,122);
   rect(0, 250, width, height/2);
-    
-  //idleAnimation.scale = 0.5;
-  animation(idleAnimation, 250, 100);
-    
-    //walkAnimation.frameDelay = 5;
-    //animation(walkAnimation, 250, 100);
-    
-   // myAnimation.changeAni('walk');
-    //myAnimation.drawAnimation('idle');
-
-    
-
-    if(kb.pressing('d'))
-    {
-        
-       //myAnimation.changeAni('walk');
-       
-       // myAnimation.vel.x = 2;
-	//	myAnimation.scale.x = 0.5;
-        
-        myAnimation.updatePosition('forward');
-        myAnimation.drawAnimation('walk');
-        
-        
-        if(myAnimation.isColliding(dogImage))
-        {
-            dogImage.remove();     
-        }     
-    }
-    else if(kb.pressing('a'))
-    {
-      //  myAnimation.changeAni('walk');
-      //  myAnimation.vel.x = -2;
-//		myAnimation.scale.x = -0.5;
-        
-        myAnimation.updatePosition('reverse');
-        myAnimation.drawAnimation('walk');
-        
-            
-    }
-    else if(kb.pressing('w'))
-    {
-      //  myAnimation.changeAni('walk');
-      //  myAnimation.vel.x = -2;
-//		myAnimation.scale.x = -0.5;
-        
-        myAnimation.updatePosition('up');
-        myAnimation.drawAnimation('walk');
-        
-            
-    }
-    else if(kb.pressing('s'))
-    {
-      //  myAnimation.changeAni('walk');
-      //  myAnimation.vel.x = -2;
-//		myAnimation.scale.x = -0.5;
-        
-        myAnimation.updatePosition('down');
-        myAnimation.drawAnimation('walk');
-        
-            
-    }
-    else
-    {
-        myAnimation.drawAnimation('idle');
-        
-  //      myAnimation.changeAni('idle');
-    //    myAnimation.vel.x = 0;
-		
-    } 
-
-
-
+  
+  //movements
+  if (kb.pressing('left')) dog.x -=5;
+  if (kb.pressing('right')) dog.x +=5;
+  if (kb.pressing('up')) dog.y -=5;
+  if (kb.pressing('down')) dog.y +=5;
+  
+  //instructions
+  fill('black');
+  text('Collect all the bones, but avoid logs and watch out for bees!', 350, 30);
+  
+  //score
+  fill('black');
+  text('Score: ' + score, 20, 30);
+  
+  //health
+  fill('black');
+  text('Health: ' + dog.health, 930, 30);
+  
+  //dog health + game over
+  if (dog.health == 0) {
+    fill('black');
+    rect(0, 0, width, height);
+    textAlign(CENTER);
+    textSize(40);
+    stroke('black');
+    fill('white');
+    text("GAME OVER", width/2, height/2-50);
+    text("Better luck next time!", width/2, height/2+50);
+  }  
+  
 }
 
+/////////////////////////////////////////////////////////
+function imageSequence(prefix, numberOfFrames, ext=".png") {
+  let sequence = [];
+  for (let i = 0; i < numberOfFrames; i++) {
+    sequence[i] = prefix + i + ext;
+  }
+  return sequence;
+}
+
+////////////////////////////////////////////////////////////
+function boneHit(dog, bone) {
+  score++;
+  bone.remove();
+}
+
+///////////////////////////////////////////////////////
+function logHit(dog, log) {
+}
+
+/////////////////////////////////////////////////////
+function beeHit(dog, bee) { 
+  dog.health--;
+  bee.remove();
+}
+
+//END//
